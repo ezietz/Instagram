38c081a656690b4ad09c6a15398b2ea4d522a4fd
Elizabeth Zietz <ezietz@fb.com>
2019-07-09 12:58:51 -0700

    Added tab bar

---
+++ Instagram/View Controllers/CameraViewController.h
@@ -0,0 +1,17 @@
+//
+//  CameraViewController.h
+//  Instagram
+//
+//  Created by ezietz on 7/9/19.
+//  Copyright © 2019 ezietz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CameraViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
